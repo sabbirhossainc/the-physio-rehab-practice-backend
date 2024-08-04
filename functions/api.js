b59e6a6028abc510ce8b94e56bcc1a6c6f025883
Md. Sabbir Hossain <shr472978@gmail.com>
2024-08-05 03:06:18 +0600

    v1

--- functions/api.js
+++ functions/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+const testData = require('../db/v1.json');
 
 let records = [];
 
@@ -11,40 +12,25 @@ router.get('/', (req, res) => {
 });
 
 //Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
+// router.post('/add', (req, res) => {
+//   res.send('New record added.');
+// });
 
 //delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
+// router.delete('/', (req, res) => {
+//   res.send('Deleted existing record');
+// });
 
 //updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
+// router.put('/', (req, res) => {
+//   res.send('Updating existing record');
+// });
 
 //showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
+router.get('/v1', (req, res) => {
+  res.json(testData);
 });
 
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
+
